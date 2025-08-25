f786d0aa11dbe928c2e4c40be96f09eaaf3d2970
Martin Liu <144648079+Maziliu@users.noreply.github.com>
2025-08-24 22:32:11 -0600

    refactored event submit seciton

--- dailies/lib/ui/views/calendar/sub page/sections/submit_section.dart
+++ dailies/lib/ui/views/calendar/sub page/sections/submit_section.dart
@@ -13,7 +13,6 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 class SubmitSection extends StatelessWidget {
   final GlobalKey<FormBuilderState> _detailsFormKey, _patternFormKey;
   final AddEventFacade _viewModelFacade;
-  final DateTime _selectedDay;
 
   const SubmitSection({
     super.key,
@@ -23,31 +22,7 @@ class SubmitSection extends StatelessWidget {
     required DateTime selectedDay,
   }) : _detailsFormKey = detailsFormKey,
        _patternFormKey = patternFormKey,
-       _viewModelFacade = viewModelFacade,
-       _selectedDay = selectedDay;
-
-  Event _onPressedEventCreate(Map detailFields, Map? patternFields) {
-    final String eventName = detailFields[EVENT_NAME_FIELD_TAG]?.value;
-    final String? locationName = detailFields[LOCATION_NAME_FIELD_TAG]?.value;
-
-    final Event event = Event(eventName: eventName, location: locationName);
-
-    final TimeSlot referenceTimeSlot = _viewModelFacade.eventTypeSectionViewModel.constructTimeSlot(_selectedDay);
-    TimeSlotPattern pattern = TimeSlotPattern.UnSaved(anchorPointsList: [referenceTimeSlot]);
-
-    if (patternFields != null) {
-      final int? frequencyValue = int.tryParse(patternFields[FREQUENCY_FIELD_TAG]?.value ?? '');
-      final FrequencyType? frequencyType = patternFields[FREQUENCY_TYPE_FIELD_TAG]?.value;
-      final DateTime? patternEndDate = patternFields[END_DATE_FIELD_TAG]?.value;
-
-      pattern = _viewModelFacade.patternDetailsSectionViewModel.constructTimeSlotPattern(frequencyValue, frequencyType, patternEndDate, referenceTimeSlot);
-    }
-
-    event.pattern = pattern;
-    event.timeSlots.addAll(pattern.anchorPointsList);
-
-    return event;
-  }
+       _viewModelFacade = viewModelFacade;
 
   @override
   Widget build(BuildContext context) {
@@ -59,25 +34,11 @@ class SubmitSection extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: ElevatedButton(
         onPressed: () {
-          final bool hasPattern = _viewModelFacade.patternDetailsSectionViewModel.patternSectionToggle.value;
+          Event? event = _viewModelFacade.createEvent(detailsFormKey: _detailsFormKey, patternFormKey: _patternFormKey);
 
-          if ((_detailsFormKey.currentState?.validate() ?? false) == false) {
-            showErrorSnackbar(exception: IncompleteFormException(specificForm: 'Event Details'));
-            return;
+          if (event != null) {
+            Navigator.pop(context, event);
           }
-          if ((_patternFormKey.currentState?.validate() ?? false) == false && hasPattern) {
-            showErrorSnackbar(exception: IncompleteFormException(specificForm: 'Event Pattern'));
-            return;
-          }
-
-          final Map? detailFields = _detailsFormKey.currentState?.fields;
-          final Map? patternFields = _patternFormKey.currentState?.fields;
-
-          Event event = _onPressedEventCreate(detailFields!, patternFields);
-
-          print(event);
-
-          Navigator.pop(context, event);
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: colorScheme.primary,

b125b89ea69510fd3925dc4fbca1c5fa54d9d3ca
Martin Liu <144648079+Maziliu@users.noreply.github.com>
2025-09-05 14:25:30 -0600

    remove notif service was broken

--- dailies/lib/service/repository/stamina_repository_service.dart
+++ dailies/lib/service/repository/stamina_repository_service.dart
@@ -3,15 +3,11 @@ import 'package:dailies/common/utils/result_helpers.dart';
 import 'package:dailies/data/models/app_model.dart';
 import 'package:dailies/data/models/stamina.dart';
 import 'package:dailies/data/repositories/stamina_repository.dart';
-import 'package:dailies/service/notification/notification_service.dart';
 
 class StaminaRepositoryService {
   final StaminaRepository _repository;
-  final NotificationService _notificationService;
 
-  StaminaRepositoryService({required StaminaRepository repository, required NotificationService notificationService})
-    : _repository = repository,
-      _notificationService = notificationService;
+  StaminaRepositoryService({required StaminaRepository repository}) : _repository = repository;
 
   Future<Result<List<Stamina>>> fetchAllStaminas() async {
     Result<List<AppModel>> results = await _repository.getAllStamina();
@@ -19,39 +15,11 @@ class StaminaRepositoryService {
     return performOperationOnResultIfNotError(results, (resultList) => resultList.map((result) => result as Stamina).toList());
   }
 
-  Future<Result<int>> saveStamina(Stamina stamina) async {
-    Result<int> staminaResult = await _repository.insert(stamina);
+  Future<Result<int>> saveStamina(Stamina stamina) async => await _repository.insert(stamina);
 
-    if (staminaResult is Ok) {
-      stamina.id = (staminaResult as Ok).value;
+  Future<Result<bool>> updateStamina(Stamina updatedStamina) async => await _repository.update(updatedStamina);
 
-      await guardedAsyncExcecute(() {
-        final DateTime notificationTime = _calculateNotificationTime(stamina);
-        return _notificationService.scheduleGachaTimerNotification(stamina, notificationTime, _calculateNotificationStamina(stamina));
-      });
-    }
-
-    return staminaResult;
-  }
-
-  Future<Result<bool>> updateStamina(Stamina updatedStamina) async {
-    Result<bool> updateResult = await _repository.update(updatedStamina);
-
-    if (updateResult is Ok) {
-      guardedAsyncExcecute(() {
-        final DateTime notificationTime = _calculateNotificationTime(updatedStamina);
-        return _notificationService.scheduleGachaTimerNotification(updatedStamina, notificationTime, _calculateNotificationStamina(updatedStamina));
-      });
-    }
-
-    return updateResult;
-  }
-
-  Future<Result<int>> deleteStamina(Stamina stamina) async {
-    guardedAsyncExcecute(() => _notificationService.cancelScheduledNotification(stamina.id));
-
-    return _repository.deleteById(stamina.id);
-  }
+  Future<Result<int>> deleteStamina(Stamina stamina) async => _repository.deleteById(stamina.id);
 
   int _calculateNotificationStamina(Stamina stamina) => (stamina.maxStamina * 0.95) ~/ 1;
 
--- dailies/lib/service/service_layer_setup.dart
+++ dailies/lib/service/service_layer_setup.dart
@@ -4,7 +4,6 @@ import 'package:dailies/data/repositories/stamina_repository.dart';
 import 'package:dailies/data/repositories/time_slot_pattern_repository.dart';
 import 'package:dailies/data/repositories/time_slot_repository.dart';
 import 'package:dailies/service/global_error_service.dart';
-import 'package:dailies/service/notification/notification_service.dart';
 import 'package:dailies/service/parsing/file_parser_service.dart';
 import 'package:dailies/service/repository/event_repository_service.dart';
 import 'package:dailies/service/repository/stamina_repository_service.dart';
@@ -13,9 +12,6 @@ import 'package:dailies/service/repository/time_slot_repository_service.dart';
 import 'package:get_it/get_it.dart';
 
 Future<void> setUpServiceLayer(GetIt injector) async {
-  //Notificaiton Service
-  injector.registerLazySingleton(() => NotificationService()..initialize());
-
   //Error Stream
   injector.registerLazySingleton(() => GlobalErrorService());
 
@@ -30,12 +26,7 @@ Future<void> setUpServiceLayer(GetIt injector) async {
     ),
   );
 
-  injector.registerLazySingleton(
-    () => StaminaRepositoryService(
-      repository: injector<StaminaRepository<DriftStamina, DriftStaminasCompanion>>(),
-      notificationService: injector<NotificationService>(),
-    ),
-  );
+  injector.registerLazySingleton(() => StaminaRepositoryService(repository: injector<StaminaRepository<DriftStamina, DriftStaminasCompanion>>()));
 
   injector.registerLazySingleton(() => TimeSlotPatternRepositoryService(patternRepository: injector<TimeSlotPatternRepository>()));
 

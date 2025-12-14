3344d0788fcb6a42a59815050a4559aabea221a0
Martin Liu <martin.liu@ucalgary.ca>
2025-12-14 02:16:50 -0700

    basic event dao api

---
+++ dailies/lib/database/daos/event_dao.dart
@@ -0,0 +1,17 @@
+import 'package:dailies_v2/database/database.dart';
+import 'package:dailies_v2/database/tables/events.dart';
+import 'package:drift/drift.dart';
+
+part 'event_dao.g.dart';
+
+@DriftAccessor(tables: [Events])
+class EventDao extends DatabaseAccessor<Database> with _$EventDaoMixin {
+  EventDao(super.db);
+
+  Future<List<Event>> getAll() => select(events).get();
+
+  Future<void> deleteEvent(int id) =>
+      (delete(events)..where((e) => e.id.equals(id))).go();
+
+  Future<int> insert(EventsCompanion data) => into(events).insert(data);
+}
